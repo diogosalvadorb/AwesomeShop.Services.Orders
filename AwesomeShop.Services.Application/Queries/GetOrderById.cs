70e61e161f0f6219c9d32ccfb1cfacb7ec95ba9c
Diogo Salvador <105182603+diogosalvadorb@users.noreply.github.com>
2024-09-09 22:50:23 -0300

    camada application, queries e commands

---
+++ AwesomeShop.Services.Application/Queries/GetOrderById.cs
@@ -0,0 +1,16 @@
+﻿using AwesomeShop.Services.Application.Dtos.ViewModels;
+using MediatR;
+using System;
+
+namespace AwesomeShop.Services.Application.Queries
+{
+    public class GetOrderById : IRequest<OrderViewModel>
+    {
+        public GetOrderById(Guid id)
+        {
+            Id = id;
+        }
+
+        public Guid Id { get; private set; }
+    }
+}

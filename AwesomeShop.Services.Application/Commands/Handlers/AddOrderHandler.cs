76a61d1bb0875fdaa81f362f9843d7db3facfa9b
diogo <diogo@DESKTOP-TEE37A0>
2024-09-16 21:39:36 -0300

    acessando consul

--- AwesomeShop.Services.Application/Commands/Handlers/AddOrderHandler.cs
+++ AwesomeShop.Services.Application/Commands/Handlers/AddOrderHandler.cs
@@ -1,7 +1,11 @@
-﻿using AwesomeShop.Services.Core.Repository;
+﻿using AwesomeShop.Services.Application.Dtos.IntegrationDto;
+using AwesomeShop.Services.Core.Repository;
 using AwesomeShop.Services.Infrastructure.MessageBus;
+using AwesomeShop.Services.Infrastructure.ServiceDiscovery;
 using MediatR;
+using Newtonsoft.Json;
 using System;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -11,15 +15,29 @@ namespace AwesomeShop.Services.Application.Commands.Handlers
     {
         private readonly IOrderRepository _orderRepository;
         private readonly IMessageBusClient _messageBus;
-        public AddOrderHandler(IOrderRepository orderRepository, IMessageBusClient messageBus)
+        private readonly IServiceDiscoveryService _serviceDiscoveryService;
+        public AddOrderHandler(IOrderRepository orderRepository, IMessageBusClient messageBus, IServiceDiscoveryService serviceDiscoveryService)
         {
             _orderRepository = orderRepository;
             _messageBus = messageBus;
+            _serviceDiscoveryService = serviceDiscoveryService;
         }
 
         public async Task<Guid> Handle(AddOrder request, CancellationToken cancellationToken)
         {
             var order = request.ToEntity();
+
+            var customerUrl = await _serviceDiscoveryService
+                .GetServiceUri("CustomerServices", $"api/customers/{order.Customer.Id}");
+
+            var httpClient = new HttpClient();
+
+            var result = await httpClient.GetAsync(customerUrl);
+            var stringResult = await result.Content.ReadAsStringAsync();
+
+            var customerDto = JsonConvert.DeserializeObject<GetCustomerByIdDto>(stringResult);
+
+            Console.WriteLine(customerDto.FullName);
 
             await _orderRepository.AddAsync(order);
 
